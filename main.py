deecd29db20ccf5ffb4491a61f6149e7bbd8e220
Janith Wathsala <janijaniwathsala@gmail.com>
2024-05-04 22:51:21 +0530

    code continue

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ root.title("TO DO APP")
 root.geometry("400x400")
 root.resizable(False,False)
 
+task_list = []
+
+def add_task(event):
+    task = task_entry.get()
+    task_entry.delete(0,tk.END)
+    if task:
+        listboox.insert(tk.END, task)
+        task_list.append(task)
+
 heading = ttk.Label(root, text="ALL TASK", font=("arial", 20, "bold"))
 heading.pack()
 
@@ -15,13 +24,15 @@ frame.pack(pady=20)
 task_entry = ttk.Entry(frame, font="arial 18", width=27)
 task_entry.pack()
 
+task_entry.bind("<Return>", add_task)
+
 frame1 = ttk.Frame(root, width=300, height=250)
 frame1.pack()
 
 listboox = tk.Listbox(frame1,width=40, height=12 ,font="arial 12")
 listboox.pack(pady=10)
 
-deleteButton = ttk.Button(root, text="DELETE")
+deleteButton= ttk.Button(root, text="DELETE")
 deleteButton.pack()
 
 root.mainloop()
