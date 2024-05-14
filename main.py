36ce5b4fd60bc005293e5c90a62b5c266944677d
Janith Wathsala <janijaniwathsala@gmail.com>
2024-05-14 12:59:33 +0530

    Add the code for task in text file and add comment for some difficult code

--- main.py
+++ main.py
@@ -10,17 +10,22 @@ task_list = []
 
 def add_task(event):
     task = task_entry.get()
-    task_entry.delete(0,tk.END)
+    task_entry.delete(0,tk.END) #task_entry delete. 1 st index is 0 to last index is END
     if task:
-        with open("tasklist.txt", "a") as file:
+        with open("tasklist.txt", "a") as file: #append task to "tasklist.txt"
             file.write(f"\n{task}")    
-        listboox.insert(tk.END, task)
-        task_list.append(task)
+        listboox.insert(tk.END, task) #Add task to list END
+        task_list.append(task) #Append task to list box
+        
+                
         
 def delete_tasks():
-    task = print(listboox.get(tk.ANCHOR))
-    listboox.delete(tk.ANCHOR)
-    task_list.remove(task)
+    task = listboox.get(tk.ANCHOR) #Get the select task from list box
+    listboox.delete(tk.ANCHOR) #delete select task from listbox
+    task_list.remove(task) #remove task from listbox
+    with open("tasklist.txt", "w") as file:
+        for task in task_list:
+            file.write(f"\n{task}")
     
 def open_task():
     with open ("tasklist.txt", "r") as file:
@@ -40,7 +45,7 @@ frame.pack(pady=20)
 task_entry = ttk.Entry(frame, font="arial 18", width=27)
 task_entry.pack()
 
-task_entry.bind("<Return>", add_task)
+task_entry.bind("<Return>", add_task) 
 
 frame1 = ttk.Frame(root, width=300, height=250)
 frame1.pack()
